b7b0111bcc8cb5e6625eb9373959596554332750
Vishesh Sood <omg.itsvishesh@gmail.com>
2017-01-22 20:11:45 -0800

    got button working

--- BigGoButton/app/src/main/java/edu/washington/vsood/biggobutton/MainActivity.java
+++ BigGoButton/app/src/main/java/edu/washington/vsood/biggobutton/MainActivity.java
@@ -2,12 +2,27 @@ package edu.washington.vsood.biggobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = 0;
+        final Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                counter++;
+                btn.setText("You have pushed me " + counter + " times!");
+            }
+        });
     }
 }
